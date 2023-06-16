0dca44c1b2249e019fd75da4b727257ead73f77c
SMemic4 <129231670+SMemic4@users.noreply.github.com>
2023-06-16 15:24:18 -0400

    Update Level_8_Python.py

--- Level_8_Python.py
+++ Level_8_Python.py
@@ -456,7 +456,27 @@ def find_multiples(integer, limit):
   
   
 ########################################################################################################################################################################              
+# Grasshopper- Check for factor
+# This function should test if the factor is a factor of base. Return true if it is a factor or false if it is not.
 
+def check_for_factor(base, factor):
+    return base % factor == 0
+
+
+########################################################################################################################################################################              
+# Convert number to reversed array of digits
+# Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+def digitize(n):
+    n = str(n)
+    les = list(n)
+    les.reverse()
+    sel = list()
+    for i in les:
+        sel.append(int(i))
+    return sel
+
+########################################################################################################################################################################              
 
 
 
