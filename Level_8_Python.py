e3638c5e0cfeb0d55c98b40fe31f62a13e4b00cd
SMemic4 <129231670+SMemic4@users.noreply.github.com>
2023-09-11 20:50:32 -0400

    Update Level_8_Python.py

--- Level_8_Python.py
+++ Level_8_Python.py
@@ -2403,12 +2403,18 @@ def flick_switch(lst):
     return les
 
 ########################################################################################################################################################################
-#
-#
+# Generate range of integers
+# Implement a function named generateRange(min, max, step), which takes three arguments and generates a range of integers from min to max, with the step. The first integer is the minimum value, the second is the maximum of the range and the third is the step
+
+def generate_range(min, max, step):
+    return list(range(min,max+1,step))
 
 ########################################################################################################################################################################
-#
-#
+# Swap Values
+# I would like to be able to pass an array with two elements to my swapValues function to swap the values.
+
+def swap_values(args): 
+    return args.reverse()
 
 ########################################################################################################################################################################
 #
